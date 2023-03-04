bfe592d840ff95ee2e5d3981e427e03a7c5b4170
maneesha_lakshani <maneeshalakshani9@gmail.com>
2023-03-05 02:14:52 +0530

    feat: crud functions

--- firebase_integration/lib/main.dart
+++ firebase_integration/lib/main.dart
@@ -31,7 +31,54 @@ class _BookFireBaseDemoState extends State<BookFireBaseDemo> {
 
   TextEditingController bookNameController = TextEditingController();
   TextEditingController authorController = TextEditingController();
+  bool isEditing = false;
+  bool textFieldVisibility = false;
+  String fireStoreCollectionName = "Books";
+  late Book currentBook;
 
+  getAllBooks(){
+    return FirebaseFirestore.instance.collection(fireStoreCollectionName).snapshots();
+  }
+
+  addBook() async {
+    Book book = Book(bookName: bookNameController.text, authorName: authorController.text);
+    try{
+      FirebaseFirestore.instance.runTransaction(
+         (Transaction transaction) async {
+            await FirebaseFirestore.instance.collection(fireStoreCollectionName).doc().set(book.toJson());
+         }
+      );
+    }catch(e){
+      print(e.toString());
+    }
+  }
+
+  updateBook(Book book, String bookName, String authorName){
+    try{
+      FirebaseFirestore.instance.runTransaction((transaction) async {
+        await transaction.update(book.documentReference, {'bookName': bookName, 'authorName': authorName});
+      });
+    }catch(e){
+      print(e.toString());
+    }
+  }
+
+  updateIfEditing(){
+    if(isEditing){
+      updateBook(currentBook, bookNameController.text, authorController.text);
+      setState(() {
+        isEditing = false;
+      });
+    }
+  }
+  
+  deleteBook(Book book){
+    FirebaseFirestore.instance.runTransaction(
+      (Transaction transaction) async {
+        await transaction.delete(book.documentReference);
+      }
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
